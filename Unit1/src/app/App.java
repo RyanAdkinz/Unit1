02c61106c4d3db689e7466827134091e82d5a64d
RyanAdkinz <814476@mystma.org>
2020-04-08 19:51:23 +0000

    Added top 5 movies

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,6 +2,11 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
+        System.out.println("1. Crooked Arrows");
+        System.out.println("2. Fast and Furious Series");
+        System.out.println("3. Central Intelligence");
+        System.out.println("4. Get Hard");
+        System.out.println("5. Grown Ups");
+
     }
 }
