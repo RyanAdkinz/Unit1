e76d2cd3f93d0fbd62b4c3476f9339a9cb050081
RyanAdkinz <814476@mystma.org>
2020-04-09 19:38:49 +0000

    8 Data Types Added

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,11 +2,36 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Crooked Arrows");
-        System.out.println("2. Fast and Furious Series");
-        System.out.println("3. Central Intelligence");
-        System.out.println("4. Get Hard");
-        System.out.println("5. Grown Ups");
+        boolean livesinStMikes = true;
+        byte FavNum = 10;
+        char FirstLetterOfLastName = 'A';
+        short NumOfSib = 1;
+        int numoftimes = 10000;
+        long Othernum = 10000000;
+        float Pie = 3.14F;
+        double Deci = .88;
+
+
+
+
+        System.out.println("What is my face-off percent in decimal form?");    
+        System.out.println(Deci);
+        System.out.println("What is pie summed up?");    
+        System.out.println(Pie);
+        System.out.println("How many times do I think of lacrosse daily?");    
+        System.out.println(Othernum);
+        System.out.println("How many times do I think of my girlfriend daily?");    
+        System.out.println(numoftimes);
+        System.out.println("How many siblings do I have?");    
+        System.out.println(NumOfSib);
+        System.out.println("What is the first letter of my last name?");    
+        System.out.println(FirstLetterOfLastName);
+        System.out.println("What is my favorite number?");    
+        System.out.println(FavNum);
+        System.out.println("Do I live in St.Michael?");
+        System.out.println(livesinStMikes);
+
 
     }
+     
 }
