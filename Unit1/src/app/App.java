72d79730f645de2d049822927d288ca9fa751358
RyanAdkinz <814476@mystma.org>
2020-04-14 18:53:43 +0000

    Boolean

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,32 +2,30 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "Ryan Adkins";   
-        String verb1 = "Lacrosse";
-        String verb2 = "Sleeping";
-        String verb3 = "Eat";
-        String person2 = "Evan";
-        String person3 = "Becca";
-        String place1 = "Flordia";
-        String place2 = "STMA Activity Center";
-        String food = "Chipotle";
-        String dayofweek = "Saturday";
-        String adj1 = "Fun";
-        String adj2 = "Boring";
+        int count = 0;
+        String color = "red";
+        boolean T = true;
 
+        while (count <=10) {
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
 
-        System.out.println(person1 + " likes to play " + verb1);
-        System.out.println(person2 + " skipped class and was caught " + verb2);
-        System.out.println(person3 + " went to a nice resturant to " + verb3);
-        System.out.println(person3 + " went on vacation to " + place1);
-        System.out.println(person1 + " works at the " + place2);
-        System.out.println(person1 + " really likes eating " + food);
-        System.out.println(person2 + " really likes " + dayofweek);
-        System.out.println(person3 + " thinks tubing is very " + adj1);
-        System.out.println(person1 + " thinks school is very " + adj2);
+        System.out.println("Count finished");
 
+        while (color == "red"){
+            System.out.println("The color is " + color);
+            color = "green";
+        }
 
+        System.out.println("Color finished");
 
+        while (T == true){
+            System.out.println("Ryan Adkins likes to play lacrosse " + T);
+            T = false;
+        }
+
+        System.out.println("True statement finished");
 
     }
      
