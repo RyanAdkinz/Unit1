8fa8f33f38638fe27be174e088d9669adbee7996
RyanAdkinz <814476@mystma.org>
2020-04-17 18:08:08 +0000

    Mastery

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -9,20 +9,20 @@ public class App {
         for (int j = 2; j <= 32;  j = j * 2)
             System.out.println( +j);
 
-        System.out.println("3");
-        System.out.println("-9");
-        System.out.println("-81");
-        
-        //I have no clue how to do powers
+        for (int j = 3; j >= -81;  j = j * -j)
+            System.out.println( +j);
 
         for (int j = 50; j >= 5;  j = j - 5)
             System.out.println( +j);
 
+        for (int j = 1; j <= 9;  j = j +1){
+                System.out.println(j * j);
+            System.out.println(j + 3);
+        System.out.println(j);
+        
 
-
-
-
+        
         }
 
-     
+    }
 }
