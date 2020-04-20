79bd3f2858b8d8116d4f6f4b2066999c120653b9
RyanAdkinz <814476@mystma.org>
2020-04-20 19:44:33 +0000

    Divisable by 4

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,26 +3,17 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 22; i <= 28;  i = i + 3)
-            System.out.println( +i);
+       for (int i = 1; i <= 20; i++) {
 
-        for (int j = 2; j <= 32;  j = j * 2)
-            System.out.println( +j);
-
-        for (int j = 3; j >= -81;  j = j * -j)
-            System.out.println( +j);
-
-        for (int j = 50; j >= 5;  j = j - 5)
-            System.out.println( +j);
-
-        for (int j = 1; j <= 9;  j = j +1){
-                System.out.println(j * j);
-            System.out.println(j + 3);
-        System.out.println(j);
-        
-
-        
+          if (i % 4 == 0){
+        System.out.println("Divisable by 4");
+          } else
+       {
+       System.out.println("Not");
         }
 
-    }
-}
+        }
+
+     }
+     
+ }
