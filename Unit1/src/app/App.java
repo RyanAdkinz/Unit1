85661bbe2f58cc50a2dd64af71af4a1553966152
RyanAdkinz <814476@mystma.org>
2020-04-21 20:08:09 +0000

    Practice Challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,17 +3,62 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-       for (int i = 1; i <= 20; i++) {
+       for (int i = 0; i <= 10; i = i + 1){
 
-          if (i % 4 == 0){
-        System.out.println("Divisable by 4");
-          } else
-       {
-       System.out.println("Not");
+        
+            System.out.println(i);
+        }
+     
+     {
+
+        for (int i = 0; i < 5; i++){
+
+            System.out.println("10");
         }
 
-        }
+
+
 
      }
      
+     
+     
+        {
+
+       for (int i = 11; i <= 19; i = i + 1){
+
+        
+            System.out.println(i);
+            }
+        
+        
+            System.out.println("End First Loop");
+            System.out.println("Start Second Loop");
+        }
+
+        {
+            System.out.println("-1");
+       for (int i = 1; i <= 8; i = i + 1){
+
+        
+
+            System.out.println(i);
+        }
+
+       
+       
+       
+        for (int i = 10; i <= 19; i = i + 1){
+
+        
+            System.out.println(i);
+        }
+
+        System.out.println("End Second Loop");
+
+
+
+        
+        }
+    }
  }
