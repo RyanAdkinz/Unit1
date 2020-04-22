8b8bc51cd358f772d73c00cfe9d0a00406f304ba
RyanAdkinz <814476@mystma.org>
2020-04-22 18:26:08 +0000

    Day 17 Practice Problem

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,60 +3,33 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-       for (int i = 0; i <= 10; i = i + 1){
+       for (int i = 20; i >= 1; i = i - 1){
 
         
             System.out.println(i);
-        }
-     
-     {
-
-        for (int i = 0; i < 5; i++){
-
-            System.out.println("10");
-        }
-
-
-
-
-     }
-     
-     
-     
-        {
-
-       for (int i = 11; i <= 19; i = i + 1){
-
-        
+            if (i== 10)
+                for (int y = 0; y < 7; y++)
             System.out.println(i);
-            }
+        }
+     
         
         
             System.out.println("End First Loop");
             System.out.println("Start Second Loop");
-        }
+        
 
         {
-            System.out.println("-1");
-       for (int i = 1; i <= 8; i = i + 1){
-
         
+       for (int i = 0; i <= 36; i = i + 4){
+        System.out.println(i);
+        if (i % 12 == 0){
 
-            System.out.println(i);
+            System.out.println("Hey!");
         }
-
-       
-       
-       
-        for (int i = 10; i <= 19; i = i + 1){
-
         
-            System.out.println(i);
-        }
-
-        System.out.println("End Second Loop");
-
-
+       }
+      
+       System.out.println("End Second Loop");
 
         
         }
