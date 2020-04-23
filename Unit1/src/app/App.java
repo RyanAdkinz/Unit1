e5c4262d7af8d087e3c8ab90922f68d56602ccc5
RyanAdkinz <814476@mystma.org>
2020-04-23 19:58:45 +0000

    Day 18 Challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,35 +3,30 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
+
+        System.out.println("Double or single digit?");
+
        for (int i = 20; i >= 1; i = i - 1){
 
-        
-            System.out.println(i);
-            if (i== 10)
-                for (int y = 0; y < 7; y++)
-            System.out.println(i);
+            if (i > 10)
+            System.out.println(i + " is double digit");
+            if (i < 10)
+            System.out.println(i + " is sigle digit");
         }
+     {
+         System.out.println("Nice!");
+     }
+     {
+         System.out.println("Fast food");
+     }
      
-        
-        
-            System.out.println("End First Loop");
-            System.out.println("Start Second Loop");
-        
+        for (int j= 0; j <= 4; j= j + 1){
 
-        {
-        
-       for (int i = 0; i <= 36; i = i + 4){
-        System.out.println(i);
-        if (i % 12 == 0){
+        System.out.println("Burgers");
+        System.out.println("Fries");
 
-            System.out.println("Hey!");
+     }
+     System.out.println("Nice!");
         }
-        
-       }
-      
-       System.out.println("End Second Loop");
-
-        
-        }
-    }
+    
  }
