c0b70ad321a544cbfe9e5d656c3aef64d372184c
RyanAdkinz <814476@mystma.org>
2020-04-29 16:53:02 +0000

    Password

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,33 +8,14 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("You should enter your first name buddy");
-        String name = input.nextLine();
+        System.out.println("What's the password?");
+        String enteredPassword = input.nextLine();
 
-        System.out.println("Wassup, " + name);
+        if (enteredPassword.equals("183726073"))
+        System.out.println("Access Granted");
 
-         System.out.println("Wanna enter your last?");
-        String last = input.nextLine();
-
-        System.out.println("I like your last name, " + last);
-
-         System.out.println("How old are you?");
-        String age = input.nextLine();
-
-        System.out.println("You're only " + age + "?");
-
-         System.out.println("Are you a freshman, sophomore, junior, or senior?");
-        String grade = input.nextLine();
-
-        System.out.println("You must know a lot if you're a " + grade);
-
-         System.out.println("What's your favorite color?");
-        String color = input.nextLine();
-
-        System.out.println(color + " is a great color!");
-
-
-
+        else
+        System.out.println("Access Denied");
 
 
     }
