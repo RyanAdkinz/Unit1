610b0a958e379e226322fa6e233f2c8be0c249de
RyanAdkinz <814476@mystma.org>
2020-05-05 16:45:37 +0000

    3 Q Quiz 5/5/2020

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -8,14 +8,29 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("What's the count?");
+        System.out.println("How many inches are in a foot?");
         String num = input.nextLine();
 
-       System.out.println(">" + num + "(user entered)");
-        
-       for (int i= 1; i<= 7; i++) {
-           System.out.println(">" + i);
-       }
+        if (num.equals ("12")) {
+        System.out.println("CORRECT"); }
+        else {
+        System.out.println("INCORRECT"); }
+
+        System.out.println("What is the capitol of MN?");
+        String Cap = input.nextLine();
+
+        if (Cap.equals ("St. Paul")) {
+        System.out.println("CORRECT"); }
+        else {
+        System.out.println("INCORRECT"); }
+
+        System.out.println("What year is it");
+        String Yr = input.nextLine();
+
+        if (Yr.equals ("2020")) {
+        System.out.println("CORRECT"); }
+        else {
+        System.out.println("INCORRECT"); }
   
     }
  }
