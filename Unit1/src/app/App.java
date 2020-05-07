b2514a5751a75801386a5926ba3cf78150e8f1f5
RyanAdkinz <814476@mystma.org>
2020-05-07 18:52:08 +0000

    5/7/2020

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,31 +6,51 @@ public class App {
     public static void main(String[] args) throws Exception {
 
 
+        String nme;
+        String adj;
+        String ani;
+        String age;
+        String ver;
+        String foo;
+        String spt;
+        String act;
+        String plc;
+        String brd;
+        
         Scanner input = new Scanner(System.in);
 
-        System.out.println("How many inches are in a foot?");
-        String num = input.nextLine();
+        
+        System.out.println("Please enter your name.");
+         nme = input.nextLine();
 
-        if (num.equals ("12")) {
-        System.out.println("CORRECT"); }
-        else {
-        System.out.println("INCORRECT"); }
+        System.out.println("Please enter your age.");
+         age = input.nextLine();
 
-        System.out.println("What is the capitol of MN?");
-        String Cap = input.nextLine();
+        System.out.println("Please enter an adjective.");
+        adj = input.nextLine();
 
-        if (Cap.equals ("St. Paul")) {
-        System.out.println("CORRECT"); }
-        else {
-        System.out.println("INCORRECT"); }
+        System.out.println("Please enter an animal.");
+        ani = input.nextLine();
 
-        System.out.println("What year is it");
-        String Yr = input.nextLine();
+        System.out.println("Please enter a verb.");
+        ver = input.nextLine();
 
-        if (Yr.equals ("2020")) {
-        System.out.println("CORRECT"); }
-        else {
-        System.out.println("INCORRECT"); }
-  
+        System.out.println("Please enter a food.");
+        foo = input.nextLine();
+
+        System.out.println("Please enter a sport.");
+        spt = input.nextLine();
+
+        System.out.println("Please enter your favorite activity.");
+        act = input.nextLine();
+
+        System.out.println("Please enter a place.");
+        plc = input.nextLine();
+
+        System.out.println("Please enter a shoe brand.");
+        brd = input.nextLine();
+
+        System.out.println(nme + age + adj + ani + ver +  foo + spt + act + plc + brd);
+      
     }
  }
