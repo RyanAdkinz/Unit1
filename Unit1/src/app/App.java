88215070ff33805ba6b72991693a08f94b0780f5
RyanAdkinz <814476@mystma.org>
2020-05-18 17:39:26 +0000

    5/18/2020

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,35 +5,35 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        Dog dog3 = new Dog();
+        Scanner input = new Scanner (System.in);
 
-        int num1;
-        int num2;
-        String print;
-     
-        Scanner input = new Scanner(System.in);
-               
-        System.out.println("Please a number");
-         num1 = input.nextInt();
-
-        System.out.println("Please enter another number");
-         num2 = input.nextInt();
-
-        System.out.println("Would you like to add, subtract, multiply, or divide these?");
-         print = input.nextLine();
-
-        {if (print == "add")
-         System.out.println(num1 + num2); }
-
-        {if (print == "subtract")
-         System.out.println(num1 - num2); }
-
-        {if (print == "multiply")
-         System.out.println(num1 * num2); }
-          
-        {if (print == "divide")
-         System.out.println(num1 / num2); }
-
+        dog1.name = "Luna";
+        dog2.name = "Cheezit";
+        dog3.name = "Roofy";
+       
+        dog1.breed = "Husky";
+        dog2.breed = "German Sheperd";
+        dog3.breed = "Yorky";
+       
+        dog1.age = "2";
+        dog2.age = "3";
+        dog3.age = "4";
 
        
+        System.out.println(dog1.name);
+        System.out.println(dog1.breed);
+        System.out.println(dog1.age);
+
+        System.out.println(dog2.name);
+        System.out.println(dog2.breed);
+        System.out.println(dog2.age);
+
+        System.out.println(dog3.name);
+        System.out.println(dog3.breed);
+        System.out.println(dog3.age);
+
     }
  }
---
+++ Unit1/src/app/Dog.java
@@ -0,0 +1,12 @@
+package app;
+
+import java.text.StringCharacterIterator;
+
+public class Dog{
+
+
+    String name;
+    String age;
+    String breed;
+
+}
